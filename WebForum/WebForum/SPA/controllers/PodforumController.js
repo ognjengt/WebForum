167f671207e46c53f287345822e316cf4d123c48
Ognjen Gatalo <ognjeng95@gmail.com>
2017-06-23 11:56:20 +0200

    Namestio dugme i popup za dodavanje novog podforuma, to se prikazuje samo adminstratorima

--- WebForum/WebForum/SPA/controllers/PodforumController.js
+++ WebForum/WebForum/SPA/controllers/PodforumController.js
@@ -1,5 +1,5 @@
 ﻿webForum.controller('PodforumController', function ($scope, PodforumiFactory, $routeParams) {
 
     $scope.nazivPodforuma = $routeParams.naziv;
-
+    $scope.dodavanjePopupVisible = false;
 });
